1d5ad325541b7dbb3da23f5a1bb00a079070c824
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-07-10 12:39:43 +0100

    Alphabetise terraform variables file and add types (no changes to actual variables)

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -1,38 +1,129 @@
-variable "cluster" {}
-variable "namespace" {}
-variable "environment" {}
-variable "azure_credentials_json" { default = null }
-variable "azure_resource_prefix" {}
-variable "config_short" {}
-variable "service_short" {}
-variable "deploy_azure_backing_services" { default = true }
-variable "rg_name" {}
-variable "enable_postgres_ssl" { default = true }
+variable "app_config_file" {
+  type    = string
+  default = "config/app_config.yml"
+}
+
+variable "app_docker_image" {
+  type = string
+}
+
+variable "app_name" {
+  type    = string
+  default = null
+}
+
+variable "app_name_suffix" {
+  type    = string
+  default = null
+}
+
+variable "azure_credentials_json" {
+  type    = string
+  default = null
+}
+
+variable "azure_maintenance_window" {
+  type    = map
+  default = null
+}
+
+variable "azure_resource_prefix" {
+  type = string
+}
 
 variable "azure_sp_credentials_json" {
   type    = string
   default = null
 }
 
-variable "key_vault_name" {}
-variable "key_vault_infra_secret_name" {}
-variable "key_vault_app_secret_name" {}
-variable "app_name_suffix" { default = null }
-variable "postgres_version" { default = 14 }
-variable "app_name" { default = null }
-variable "app_docker_image" {}
-variable "enable_monitoring" { default = true }
-variable "app_config_file" { default = "config/app_config.yml" }
-variable "azure_maintenance_window" { default = null }
-variable "postgres_flexible_server_sku" { default = "B_Standard_B1ms" }
-variable "postgres_enable_high_availability" { default = false }
-variable "startup_command" {}
-variable "probe_path" { default = null }
-variable "replicas" { default = 1 }
-variable "memory_max" { default = "1Gi" }
+variable "cluster" {
+  type = string
+}
+
+variable "config_short" {
+  type = string
+}
+
+variable "deploy_azure_backing_services" {
+  type    = bool
+  default = true
+}
+
+variable "enable_monitoring" {
+  type    = bool
+  default = true
+}
+
+variable "enable_postgres_ssl" {
+  type    = bool
+  default = true
+}
+
+variable "environment" {
+  type = string
+}
+
 variable "gov_uk_host_names" {
   default = []
   type    = list(any)
+}
+
+variable "key_vault_app_secret_name" {
+  type = string
+}
+
+variable "key_vault_infra_secret_name" {
+  type = string
+}
+
+variable "key_vault_name" {
+  type = string
+}
+
+variable "memory_max" {
+  type    = string
+  default = "1Gi"
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "postgres_enable_high_availability" {
+  type    = string
+  default = false
+}
+
+variable "postgres_flexible_server_sku" {
+  type    = string
+  default = "B_Standard_B1ms"
+}
+
+variable "postgres_version" {
+  type    = number
+  default = 14
+}
+
+variable "probe_path" {
+  type    = string
+  default = null
+}
+
+variable "replicas" {
+  type    = number
+  default = 1
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "service_short" {
+  type = string
+}
+
+variable "startup_command" {
+  type = list(string)
 }
 
 locals {
@@ -43,14 +134,13 @@ locals {
 
   postgres_ssl_mode = var.enable_postgres_ssl ? "require" : "disable"
 
-  app_name_suffix  = var.app_name == null ? var.environment : var.app_name
-  kv_app_secrets    = yamldecode(data.azurerm_key_vault_secret.app_secrets.value)
-  infra_secrets     = yamldecode(data.azurerm_key_vault_secret.infra_secrets.value)
-  app_config        = yamldecode(file(var.app_config_file))[var.environment]
+  app_name_suffix = var.app_name == null ? var.environment : var.app_name
+  kv_app_secrets  = yamldecode(data.azurerm_key_vault_secret.app_secrets.value)
+  infra_secrets   = yamldecode(data.azurerm_key_vault_secret.infra_secrets.value)
+  app_config      = yamldecode(file(var.app_config_file))[var.environment]
 
   app_env_values = merge(
     local.app_config,
-    #  sslmode not defined in database.yml?
     { DB_SSLMODE = local.postgres_ssl_mode }
   )
 
@@ -59,7 +149,7 @@ locals {
   app_secrets = merge(
     local.kv_app_secrets,
     {
-      DATABASE_URL        = module.postgres.url
+      DATABASE_URL = module.postgres.url
     }
   )
 }
