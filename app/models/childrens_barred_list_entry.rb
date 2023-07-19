72b905a9b957c304be3f8f76aee57b0f4f2e9cc9
Steve Laing <steve.laing@gmail.com>
2023-07-19 17:21:30 +0100

    Add uniqueness validation for last_name scoped to first_names and date_of_birth

--- app/models/childrens_barred_list_entry.rb
+++ app/models/childrens_barred_list_entry.rb
@@ -1,6 +1,10 @@
 class ChildrensBarredListEntry < ApplicationRecord
   validates :first_names, presence: true
-  validates :last_name, presence: true
+  validates :last_name,
+            presence: true,
+            uniqueness: {
+              scope: %i[first_names date_of_birth]
+            }
   validates :date_of_birth, presence: true
 
   def self.includes_record?(last_name:, date_of_birth:)
