846977ab267721f649335ea2d2989b9ebdf37252
Steve Laing <steve.laing@gmail.com>
2023-07-25 15:10:39 +0100

    Add dsi_users table and model

---
+++ app/models/dsi_user.rb
@@ -0,0 +1,13 @@
+class DsiUser < ApplicationRecord
+  def self.create_or_update_from_dsi(dsi_payload)
+    dsi_user = find_or_initialize_by(email: dsi_payload.info.fetch(:email))
+
+    dsi_user.update!(
+      first_name: dsi_payload.info.first_name,
+      last_name: dsi_payload.info.last_name,
+      uid: dsi_payload.uid
+    )
+
+    dsi_user
+  end
+end
