870342aa2e3acc55bc9d5ad1c0bbe30bf4ceda86
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-07-31 20:09:26 +0300

    Add 'Year must be 4 numbers' to DayMonthYearValidator

--- app/validators/day_month_year_validator.rb
+++ app/validators/day_month_year_validator.rb
@@ -4,17 +4,19 @@ class DayMonthYearValidator
   def validate(record, attribute)
     @record = record
 
+    return true unless all_present?
+
     record.errors.add(attribute, :invalid) unless valid?
+    record.errors.add(attribute, :invalid_year) unless year_valid?
   end
 
   private
 
-  def valid?
-    unless @record.day.present? && @record.month.present? &&
-             @record.year.present?
-      return true
-    end
+  def all_present?
+    @record.day.present? && @record.month.present? && @record.year.present?
+  end
 
+  def valid?
     day = @record.day.to_i
     month = @record.month.to_i
     year = @record.year.to_i
@@ -24,5 +26,9 @@ class DayMonthYearValidator
     Date.valid_date?(year, month, day)
   rescue ArgumentError
     false
+  end
+
+  def year_valid?
+    @record.year.length == 4
   end
 end
