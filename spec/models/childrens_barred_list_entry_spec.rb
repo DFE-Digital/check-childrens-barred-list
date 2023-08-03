0d8d9059db2499f3a2568128e7a6108463622fd7
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-08-03 14:42:44 +0300

    Display the data from the record rather than the user input

--- spec/models/childrens_barred_list_entry_spec.rb
+++ spec/models/childrens_barred_list_entry_spec.rb
@@ -5,83 +5,83 @@ RSpec.describe ChildrensBarredListEntry, type: :model do
   it { is_expected.to validate_presence_of(:last_name) }
   it { is_expected.to validate_presence_of(:date_of_birth) }
 
-  describe "#self.includes_record?" do
+  describe "#self.search" do
     let(:record) { create(:childrens_barred_list_entry) }
 
     context "with an exact match" do
-      it "returns true" do
+      it "returns the record" do
         expect(
-          described_class.includes_record?(
+          described_class.search(
             last_name: record.last_name,
             date_of_birth: record.date_of_birth,
           ),
-        ).to be_truthy
+        ).to eq(record)
       end
     end
 
     context "with a case-insensitive match on last_name" do
-      it "returns true" do
+      it "returns the record" do
         expect(
-          described_class.includes_record?(
+          described_class.search(
             last_name: record.last_name.downcase,
             date_of_birth: record.date_of_birth,
           ),
-        ).to be_truthy
+        ).to eq(record)
       end
     end
 
     context "with an accent-insensitive match on last_name" do
       it "returns true" do
         expect(
-          described_class.includes_record?(
+          described_class.search(
             last_name: "Doé",
             date_of_birth: record.date_of_birth,
           ),
-        ).to be_truthy
+        ).to eq(record)
       end
     end
 
     context "with a whitespace insensitive match on last_name" do
       it "returns true" do
         expect(
-          described_class.includes_record?(
+          described_class.search(
             last_name: " Doe ",
             date_of_birth: record.date_of_birth,
           ),
-        ).to be_truthy
+        ).to eq(record)
       end
     end
 
     context "with matching DoB but no matching last_name" do
-      it "returns false" do
+      it "returns nil" do
         expect(
-          described_class.includes_record?(
+          described_class.search(
             last_name: "Random name",
             date_of_birth: record.date_of_birth,
           ),
-        ).to be_falsey
+        ).to be_nil
       end
     end
 
     context "with matching last_name but no matching DoB" do
-      it "returns false" do
+      it "returns nil" do
         expect(
-          described_class.includes_record?(
+          described_class.search(
             last_name: record.last_name,
             date_of_birth: Time.zone.today,
           ),
-        ).to be_falsey
+        ).to be_nil
       end
     end
 
     context "with no matching DoB or last_name" do
-      it "returns false" do
+      it "returns nil" do
         expect(
-          described_class.includes_record?(
+          described_class.search(
             last_name: "Random name",
             date_of_birth: Time.zone.today,
           ),
-        ).to be_falsey
+        ).to be_nil
       end
     end
   end
