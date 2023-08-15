55550d06da2f63729f50da6a3ba29afa12063d48
Steve Laing <steve.laing@gmail.com>
2023-08-15 16:56:41 +0100

    Populate the upload file hash attribute on the UplaodForm
    We use this to constrain what we will see as part of the preview. This prevents simultaneous uploads affecting one another.

--- app/forms/support_interface/upload_form.rb
+++ app/forms/support_interface/upload_form.rb
@@ -2,14 +2,16 @@ module SupportInterface
   class UploadForm
     include ActiveModel::Model
 
-    attr_accessor :file
+    attr_accessor :file, :upload_file_hash
 
     validates :file, presence: true
 
     def save
       return false unless valid?
 
-      CreateChildrensBarredListEntries.new(file.read).call
+      service = CreateChildrensBarredListEntries.new(file.read)
+      @upload_file_hash = service.upload_file_hash
+      service.call
     rescue CSV::MalformedCSVError
       errors.add(:file, :invalid_csv)
       false
