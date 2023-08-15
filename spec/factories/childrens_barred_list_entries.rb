250a14bc791543bac7242f75a1b14e8afe1ca512
Steve Laing <steve.laing@gmail.com>
2023-08-15 16:49:13 +0100

    Add a service to confirm uploaded entries
    This service confirms the unconfirmed entries for a given upload file hash.

--- spec/factories/childrens_barred_list_entries.rb
+++ spec/factories/childrens_barred_list_entries.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     first_names { "John" }
     last_name { "Doe" }
     date_of_birth { 25.years.ago }
+    confirmed { true }
+    confirmed_at { 1.day.ago }
+    upload_file_hash { Digest::SHA256.hexdigest("test") }
+
+    trait :unconfirmed do
+      confirmed { false }
+      confirmed_at { nil }
+    end
   end
 end
