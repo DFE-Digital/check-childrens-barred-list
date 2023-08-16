fbdc35d55d69d8b8093d33c06d5495dfc7a460bb
Steve Laing <steve.laing@gmail.com>
2023-08-16 17:59:15 +0100

    Read db encryption credentials from ENV

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,9 @@ module CheckTheChildrensBarredList
     config.assets.paths << Rails.root.join("node_modules/govuk-frontend/dist/govuk/assets/fonts")
 
     config.active_job.queue_adapter = :sidekiq
+
+    config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+    config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+    config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
   end
 end
