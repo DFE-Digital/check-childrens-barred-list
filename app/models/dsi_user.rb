9085a0927262243863af384a8a3880c852e06770
Malcolm Baig <git@baig.org>
2023-09-18 13:59:42 +0100

    Create a DsiUserSession record when creating or updating the user
    We only do this if supplied an optional role. Auth bypass mechanisms
    will still function without a role.

--- app/models/dsi_user.rb
+++ app/models/dsi_user.rb
@@ -1,5 +1,7 @@
 class DsiUser < ApplicationRecord
-  def self.create_or_update_from_dsi(dsi_payload)
+  has_many :dsi_user_sessions, dependent: :destroy
+
+  def self.create_or_update_from_dsi(dsi_payload, role = nil)
     dsi_user = find_or_initialize_by(email: dsi_payload.info.fetch(:email))
 
     dsi_user.update!(
@@ -7,6 +9,15 @@ class DsiUser < ApplicationRecord
       last_name: dsi_payload.info.last_name,
       uid: dsi_payload.uid
     )
+
+    if role.present?
+      dsi_user.dsi_user_sessions.create!(
+        role_id: role["id"],
+        role_code: role["code"],
+        organisation_id: dsi_payload.extra.raw_info.organisation.id,
+        organisation_name: dsi_payload.extra.raw_info.organisation.name,
+      )
+    end
 
     dsi_user
   end
--- spec/models/dsi_user_spec.rb
+++ spec/models/dsi_user_spec.rb
@@ -3,7 +3,18 @@ require "rails_helper"
 RSpec.describe DsiUser, type: :model do
   describe ".create_or_update_from_dsi" do
     let(:dsi_payload) do
-      OmniAuth::AuthHash.new(uid: "123456", info: { email:, first_name: "John", last_name: "Doe" })
+      OmniAuth::AuthHash.new(
+        uid: "123456",
+        info: { email:, first_name: "John", last_name: "Doe" },
+        extra: {
+          raw_info: {
+            organisation: {
+              id: "09876",
+              name: "Test Org",
+            }
+          }
+        }
+      )
     end
     let(:email) { "test@example.com" }
 
@@ -30,6 +41,20 @@ RSpec.describe DsiUser, type: :model do
         expect(dsi_user.uid).to eq dsi_payload.uid
         expect(dsi_user.first_name).to eq dsi_payload.info.first_name
         expect(dsi_user.last_name).to eq dsi_payload.info.last_name
+      end
+    end
+
+    context "when the user has a role" do
+      it "creates a session record" do
+        role = { "id" => "123", "code" => "TestRole_code" }
+        described_class.create_or_update_from_dsi(dsi_payload, role)
+
+        dsi_user_session = DsiUser.first.dsi_user_sessions.first
+        expect(dsi_user_session).to be_present
+        expect(dsi_user_session.role_id).to eq "123"
+        expect(dsi_user_session.role_code).to eq "TestRole_code"
+        expect(dsi_user_session.organisation_id).to eq "09876"
+        expect(dsi_user_session.organisation_name).to eq "Test Org"
       end
     end
   end
