52c5e2a775035ea3825dbdd87cde861b7a1ef0b0
Malcolm Baig <git@baig.org>
2023-10-30 15:43:30 +0000

    Refactor omniauth_callbacks_controller
    Extract methods to make the flow a little clearer (including the
    difference in behaviour when bypass mode is active).
    
    This also prepares the controller for further changes related to storing
    the organisation details in the session, which we'll need to display a
    more informative 401 page when authorisation fails.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,24 +5,43 @@ class OmniauthCallbacksController < ApplicationController
   skip_before_action :authenticate_dsi_user!
   skip_before_action :handle_expired_session!
 
+  before_action :clear_session_attributes
+
   def dfe
-    auth = request.env["omniauth.auth"]
-
-    unless DfESignIn.bypass?
-      role = DfESignInApi::GetUserAccessToService.new(
-        org_id: auth.extra.raw_info.organisation.id,
-        user_id: auth.uid,
-      ).call
-
+    if DfESignIn.bypass?
+      create_or_update_dsi_user
+    else
+      role = check_user_access_to_service
       return redirect_to not_authorised_path unless role
+
+      create_or_update_dsi_user(role)
     end
-
-    @dsi_user = DsiUser.create_or_update_from_dsi(auth, role)
-
-    session[:dsi_user_id] = @dsi_user.id
-    session[:dsi_user_session_expiry] = 2.hours.from_now.to_i
 
     redirect_to root_path
   end
   alias_method :dfe_bypass, :dfe
+
+  private
+
+  def auth
+    request.env["omniauth.auth"]
+  end
+
+  def clear_session_attributes
+    session[:dsi_user_id] = nil
+    session[:dsi_user_session_expiry] = nil
+  end
+
+  def create_or_update_dsi_user(role = nil)
+    @dsi_user = DsiUser.create_or_update_from_dsi(auth, role)
+    session[:dsi_user_id] = @dsi_user.id
+    session[:dsi_user_session_expiry] = 2.hours.from_now.to_i
+  end
+
+  def check_user_access_to_service
+    DfESignInApi::GetUserAccessToService.new(
+      org_id: auth.extra.raw_info.organisation.id,
+      user_id: auth.uid,
+    ).call
+  end
 end
