b282b337d3de13cb03cc88bc6a57c739f26c7c72
Graham Pengelly <graham@three-tier.com>
2023-12-11 10:38:59 +0000

    Show support navigation to internal users
    Show/hide support navigation based on user role.

--- spec/system/user_signs_in_spec.rb
+++ spec/system/user_signs_in_spec.rb
@@ -36,12 +36,14 @@ RSpec.describe "DSI authentication", type: :system do
   def and_i_can_access_the_support_interface
     visit support_interface_root_path
     expect(page).to have_content("Features")
-    visit new_support_interface_upload_path
+    click_on "File upload"
     expect(page).to have_content("Upload records")
   end
 
   def and_i_cannot_access_the_support_interface
     not_authorised_message = "You cannot use the DfE Sign-in account for Test School to perform this action"
+    visit root_path
+    expect(page).not_to have_content("Features")
     visit support_interface_root_path
     expect(page).to have_content(not_authorised_message)
     visit new_support_interface_upload_path
