6f3cc6c795ab512e9e5f12ac997fcf1174ed6810
Steve Laing <steve.laing@gmail.com>
2024-02-07 12:50:10 +0000

    Configure exceptions app to use routes for errors controller

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,7 @@ module CheckTheChildrensBarredList
     config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
     config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
     config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+
+    config.exceptions_app = self.routes # rubocop:disable Style/RedundantSelf
   end
 end
