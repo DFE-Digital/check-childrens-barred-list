690344bee1694a910e9fcbfda75669b56c6ab318
Steve Laing <steve.laing@gmail.com>
2024-03-04 09:29:29 +0000

    Add ability to create new roles to support interface

--- app/controllers/support_interface/roles_controller.rb
+++ app/controllers/support_interface/roles_controller.rb
@@ -3,5 +3,22 @@ module SupportInterface
     def index
       @roles = Role.all
     end
+
+    def new
+      @role = Role.new
+    end
+
+    def create
+      @role = Role.create!(role_params)
+      redirect_to support_interface_roles_path
+    end
+
+    private
+
+    def role_params
+      params.require(:role).permit(:code, :enabled, :internal ).tap do |rp|
+        rp[:enabled] = ActiveRecord::Type::Boolean.new.cast(rp[:enabled])
+      end
+    end
   end
 end
