739534ff8b24e04c64bf19f87bcbda7765a782b1
Graham Pengelly <graham@three-tier.com>
2024-04-03 12:49:39 +0100

    Don't require auth for feedback
    We're currently redirecting users that haven't authenticated back to the
    start page when they click the feedback link.
    
    Don't do that.

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,4 +1,7 @@
 class FeedbacksController < ApplicationController
+  skip_before_action :authenticate_dsi_user!
+  skip_before_action :handle_expired_session!
+
   def new
     @feedback = Feedback.new
   end
