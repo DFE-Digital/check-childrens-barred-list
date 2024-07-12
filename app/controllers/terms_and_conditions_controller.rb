8ebd000c4aa42404959ea6d821a1d063977cbe9a
Richard Pattinson <richie.pattinson@googlemail.com>
2024-07-12 10:46:41 +0100

    Allow unauthenticated user to view the terms and conditions page

--- app/controllers/terms_and_conditions_controller.rb
+++ app/controllers/terms_and_conditions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TermsAndConditionsController < ApplicationController
+  skip_before_action :authenticate_dsi_user!
+  skip_before_action :handle_expired_session!
   skip_before_action :enforce_terms_and_conditions_acceptance!
 
   def show
