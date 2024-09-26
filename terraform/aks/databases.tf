d316994c96b0f658d60795131836f539617c7132
John Ake <johnake@hotmail.com>
2024-09-26 14:06:26 +0100

    Pin postgress version

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -13,7 +13,7 @@ module "postgres" {
   use_azure               = var.deploy_azure_backing_services
   azure_enable_monitoring = var.enable_monitoring
 #  azure_extensions        = ["PGCRYPTO","BTREE_GIST","CITEXT","PG_TRGM"]
-  server_version          = var.postgres_version
+  server_version          = "14"
   azure_sku_name          = var.postgres_flexible_server_sku
 
   azure_enable_high_availability = var.postgres_enable_high_availability
